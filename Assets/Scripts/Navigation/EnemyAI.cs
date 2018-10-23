b5beb86fe8eef7da9172afb723e5ee6b1ab6fc08
Chase Kurkowski <zeke1498@gmail.com>
2018-10-23 11:57:30 -0500

    Tweaks to enemy Nav

--- Assets/Scripts/Navigation/EnemyAI.cs
+++ Assets/Scripts/Navigation/EnemyAI.cs
@@ -27,6 +27,8 @@ public class EnemyAI : MonoBehaviour
     private float patrolSpeed = 1f;
     private float chaseSpeed = 1.8f;
     private float offset = 5f;
+    private float timer = 0;
+    private float maxTime = 2;
 
     //Transform for targeting
     private Transform targetedTransform;
@@ -74,15 +76,21 @@ public class EnemyAI : MonoBehaviour
         agent.speed = patrolSpeed;
         if (Vector3.Distance(transform.position, agent.destination) >= 2)
         {
-            print("Move to Pos");
             float step = patrolSpeed * Time.deltaTime;
 
             transform.position = Vector3.MoveTowards(transform.position, agent.destination, step);
         }
         else if (Vector3.Distance(transform.position, agent.destination) < 2)
         {
-            print("Search new Pos");
-            agent.destination = RandomPosition();
+            timer += Time.deltaTime;
+
+            //print(timer);
+            if (timer >= maxTime)
+            {
+                agent.destination = RandomPosition();
+                timer = 0;
+            }
+                
         }
 
         if (Vector3.Distance(target.transform.position, transform.position) <= 5)
@@ -115,25 +123,33 @@ public class EnemyAI : MonoBehaviour
         bool validPos = false;
         RaycastHit hit;
 
-        float xOffset = 5f;
-        float zOffset = 5f;
+        float xOffset = 8f;
+        float zOffset = 8f;
 
         float xPos = Random.Range(transform.position.x + xOffset, transform.position.x - xOffset);
         float zPos = Random.Range(transform.position.z + zOffset, transform.position.z - zOffset);
 
-        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        if (Physics.Raycast(new Vector3(xPos, transform.position.y, zPos), Vector3.down, out hit))
         {
             print(hit.collider.name);
             if (hit.collider.tag == "Walkable")
             {
-                //Room roomRef = hit.collider.gameObject.GetComponent<Room>();
-                //Room[] rooms = new Room[roomRef.getNeighboringRooms().Count];
-                //roomRef.getNeighboringRooms().CopyTo(rooms);
+                Room room = new Room(transform.position);
+                Room[] rooms = new Room[room.getNeighboringRooms().Count];
+                room.getNeighboringRooms().CopyTo(rooms);
 
-                //Room selectedRoom = rooms[Random.Range(0, rooms.Length)];
-                //newPos = new Vector3(selectedRoom)
+                Room selectedRoom = rooms[Random.Range(0, rooms.Length)];
+                Transform roomPos = selectedRoom.roomRef.transform;
 
-                newPos = new Vector3(xPos, transform.position.y, zPos);
+
+
+                newPos = new Vector3(roomPos.position.x, transform.position.y, roomPos.position.z);
+
+                //Vector3 room = hit.collider.transform.position;
+                //float xRoomPos = Random.Range(room.x + xOffset, room.x - xOffset);
+                //float zRoomPos = Random.Range(room.z + zOffset, room.z - zOffset);
+
+                //newPos = new Vector3(xPos, transform.position.y, zPos);
 
                 validPos = true;
             }
