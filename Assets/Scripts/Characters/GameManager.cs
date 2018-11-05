6a203c9eeaa1ce4467c20358e640f750d0f7d8ed
awisniewski21 <43355483+awisniewski21@users.noreply.github.com>
2018-11-05 16:07:07 -0600

    Level Added
    Level was added

--- Assets/Scripts/Characters/GameManager.cs
+++ Assets/Scripts/Characters/GameManager.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+    public static int level = 1;
+
+    public void LoadScene(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
 	public void EndGame()
     {
         //TODO: Game over screen
+        level = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
